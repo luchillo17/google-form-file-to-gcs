472194cdb37136549dbd747ca5c4565c3585a781
Carlos Esteban Lopez Jaramillo <luchillo17@gmail.com>
2024-03-15 23:43:02 -0500

    feat: Upload to cloud working

--- src/main.ts
+++ src/main.ts
@@ -1,46 +1,57 @@
-function getOAuthClient() {
-  const serviceAccount: ServiceAccountI = JSON.parse(
-    PropertiesService.getScriptProperties().getProperty("serviceAccount")
+let token: string;
+let serviceAccount: IServiceAccount;
+let storageService: GoogleAppsScriptOAuth2.OAuth2Service;
+let spreadSheet: GoogleAppsScript.Spreadsheet.Spreadsheet;
+
+function getServiceAccountProperty(): IServiceAccount {
+  return (
+    serviceAccount ??
+    JSON.parse(
+      PropertiesService.getScriptProperties().getProperty("serviceAccount")
+    )
   );
+}
+
+function getStorageService(): GoogleAppsScriptOAuth2.OAuth2Service {
+  const serviceAccount = getServiceAccountProperty();
+
+  return (
+    storageService ??
+    OAuth2.createService("CloudStorage")
+      .setPrivateKey(serviceAccount.private_key)
+      .setIssuer(serviceAccount.client_email)
+      .setPropertyStore(PropertiesService.getUserProperties())
+      .setCache(CacheService.getUserCache())
+      .setTokenUrl("https://oauth2.googleapis.com/token")
+      .setScope("https://www.googleapis.com/auth/devstorage.read_write")
+  );
+}
+
+function getAccessToken(): string {
+  return token ?? getStorageService().getAccessToken();
+}
+
+function getBucketData() {
   const [bucket, formFilesFolder] = PropertiesService.getScriptProperties()
     .getProperty("formFilesPath")
     .split("/");
 
-  // Construct JWT
-  const now = Math.floor(Date.now() / 1e3);
-  const jwtHeader = { alg: "RS256", typ: "JWT" };
+  if (
+    typeof bucket !== "string" ||
+    typeof formFilesFolder !== "string" ||
+    bucket.trim() === "" ||
+    formFilesFolder.trim() === ""
+  ) {
+    throw new Error(`formFilesPath invalid: ${bucket}, ${formFilesFolder}`);
+  }
 
-  const claimSet = {
-    iss: serviceAccount.client_email,
-    scope: "https://www.googleapis.com/auth/cloud-platform",
-    aud: "https://oauth2.googleapis.com/token",
-    exp: (now + 3600).toString(),
-    iat: now.toString(),
-  };
+  const accessToken = getAccessToken();
 
-  const signature =
-    Utilities.base64Encode(JSON.stringify(jwtHeader)) +
-    "." +
-    Utilities.base64Encode(JSON.stringify(claimSet));
-  const jwt =
-    signature +
-    "." +
-    Utilities.base64Encode(
-      Utilities.computeRsaSha256Signature(signature, serviceAccount.private_key)
-    );
+  if (typeof accessToken !== "string" || accessToken.trim() === "") {
+    throw new Error("Empty token");
+  }
 
-  // Get an access token
-  const tokenResponse = UrlFetchApp.fetch(
-    "https://oauth2.googleapis.com/token",
-    {
-      method: "post",
-      payload: {
-        grant_type: "urn:ietf:params:oauth:grant-type:jwt-bearer",
-        assertion: jwt,
-      },
-    }
-  );
-  const accessToken = JSON.parse(tokenResponse.getContentText()).access_token;
+  const serviceAccount = getServiceAccountProperty();
 
   const url = `https://storage.googleapis.com/storage/v1/b/${bucket}/o?project=${serviceAccount.project_id}&prefix=${formFilesFolder}`;
 
@@ -52,3 +63,121 @@ function getOAuthClient() {
 
   console.log("Result: ", result);
 }
+
+function onFormSubmit(event: {
+  response: GoogleAppsScript.Forms.FormResponse;
+}) {
+  try {
+    // Get file uploads
+    const responseItems = event.response.getItemResponses();
+
+    const fileType = FormApp.ItemType.FILE_UPLOAD;
+
+    const driveFileIds = responseItems
+      .filter((item) => item.getItem().getType() === fileType)
+      .flatMap((item) => item.getResponse() as string | string[]);
+
+    uploadDriveTo(driveFileIds);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+function uploadDriveTo(driveFileIds: string[]) {
+  if (driveFileIds.length <= 0) {
+    throw new Error("No files sent in form");
+  }
+
+  const [bucket, formFilesFolder] = PropertiesService.getScriptProperties()
+    .getProperty("formFilesPath")
+    .split("/");
+
+  if (
+    typeof bucket !== "string" ||
+    typeof formFilesFolder !== "string" ||
+    bucket.trim() === "" ||
+    formFilesFolder.trim() === ""
+  ) {
+    throw new Error(`formFilesPath invalid: ${bucket}, ${formFilesFolder}`);
+  }
+
+  const accessToken = getAccessToken();
+
+  if (typeof accessToken !== "string" || accessToken.trim() === "") {
+    throw new Error("Empty token");
+  }
+
+  for (let fileId of driveFileIds) {
+    const file = DriveApp.getFileById(fileId);
+    const blob = file.getBlob();
+    const bytes = blob.getBytes();
+
+    const url = `https://storage.googleapis.com/upload/storage/v1/b/${bucket}/o?uploadType=media&name=${formFilesFolder}/${file.getName()}`;
+
+    console.log("Url: ", url);
+
+    const response = UrlFetchApp.fetch(url, {
+      method: "post",
+      contentType: blob.getContentType(),
+      payload: bytes,
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+
+    const result: IUploadResponse = JSON.parse(response.getContentText());
+
+    console.log("Upload result: ", response.getResponseCode(), result);
+  }
+}
+
+function onFormSubmitGetResponseRow(e) {
+  console.log(e);
+  console.log(e.range);
+  console.log(e.range.getSheet());
+}
+
+function installOnFormSubmitSheet() {
+  const triggerPropertyName = "onFormSubmitSheetUniqueId";
+
+  let propTriggerId =
+    PropertiesService.getScriptProperties().getProperty(triggerPropertyName);
+
+  if (propTriggerId) {
+    const trigger = ScriptApp.getProjectTriggers().find(
+      (trigger) => trigger.getUniqueId() === propTriggerId
+    );
+
+    if (trigger) {
+      console.log(
+        `Trigger with the following unique ID already exists: ${propTriggerId}`
+      );
+      return;
+    }
+  }
+
+  // Creates the trigger if one doesn't exist.
+  const sheet = SpreadsheetApp.getActive();
+  console.log("Active Spreadsheet: ", sheet);
+  propTriggerId = ScriptApp.newTrigger("onFormSubmitSheet")
+    .forSpreadsheet(sheet)
+    .onFormSubmit()
+    .create()
+    .getUniqueId();
+
+  PropertiesService.getScriptProperties().setProperty(
+    triggerPropertyName,
+    propTriggerId
+  );
+
+  console.log(
+    `Trigger with the following unique ID was created: ${propTriggerId}`
+  );
+}
+
+// function replaceSheetFileResponse(responseRow: number, driveUrl: string, gcsUrl: string) {
+//   spreadSheet = spreadSheet ?? SpreadsheetApp.getActive();
+
+//   spreadSheet.
+
+// }
--- src/types.d.ts
+++ src/types.d.ts
@@ -1,4 +1,4 @@
-interface ServiceAccountI {
+interface IServiceAccount {
   type: string;
   project_id: string;
   private_key_id: string;
@@ -10,4 +10,24 @@ interface ServiceAccountI {
   auth_provider_x509_cert_url: string;
   client_x509_cert_url: string;
   universe_domain: string;
+}
+
+interface IUploadResponse {
+  id: string; //"employee-shared/form-files/20240315_183943 - Saray Lopez Jaramillo.heic/1710556887802846";
+  kind: string; //"storage#object";
+  selfLink: string; //"https://www.googleapis.com/storage/v1/b/employee-shared/o/form-files%2F20240315_183943%20-%20Saray%20Lopez%20Jaramillo.heic";
+  mediaLink: string; //"https://storage.googleapis.com/download/storage/v1/b/employee-shared/o/form-files%2F20240315_183943%20-%20Saray%20Lopez%20Jaramillo.heic?generation=1710556887802846&alt=media";
+  name: string; //"form-files/20240315_183943 - Saray Lopez Jaramillo.heic";
+  bucket: string; //"employee-shared";
+  generation: string; //"1710556887802846";
+  metageneration: string;
+  contentType: string; //"image/heif";
+  storageClass: string;
+  size: string;
+  md5Hash: string;
+  crc32c: string;
+  etag: string;
+  timeCreated: string;
+  updated: string;
+  timeStorageClassUpdated: string;
 }
